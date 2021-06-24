d1a86d0af81b7c897a876163b86593073d8ee9bc
ibwei <bawei3600@gmail.com>
2021-06-25 00:22:57 +0800

    feat(activity): add activity logics

--- src/components/ActivityCard/index.tsx
+++ src/components/ActivityCard/index.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import Column from '../Column'
-import { HomePageWrap } from '../../pages/home/index'
 import { useTranslation } from 'react-i18next'
 
 export interface ActivityCardProps {
@@ -12,6 +11,34 @@ export interface ActivityCardProps {
   valid?: boolean
 }
 
+const Title = styled.div`
+  font-size: 20px;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #ffffff;
+`
+export const Button = styled.div`
+  transition: all 0.3s ease-in-out;
+  opacity: 0;
+  width: auto;
+  height: 32px;
+  background: #49ffa1;
+  border-radius: 4px;
+  padding: 0 10px;
+  color: #000;
+  text-align: center;
+  line-height: 32px;
+  margin-top: 56px;
+  cursor: pointer;
+  @media (max-width: 1200px) {
+    opacity: 1;
+  }
+`
+const InvalidButton = styled(Button)`
+  background: rgba(0, 0, 0, 0.6);
+  color: #fff;
+`
+
 const ActivityCardWrap = styled(Column)`
   width: 280px;
   height: 208px;
@@ -20,34 +47,9 @@ const ActivityCardWrap = styled(Column)`
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
-`
-const Title = styled.div`
-  font-size: 20px;
-  font-family: PingFangSC-Regular, PingFang SC;
-  font-weight: 400;
-  color: #ffffff;
-`
-export const Button = styled.div`
-  width: 88px;
-  height: 32px;
-  background: #49ffa1;
-  border-radius: 4px;
-  color: #000;
-  text-align: center;
-  line-height: 32px;
-  margin-top: 56px;
-  cursor: pointer;
-  &:hover {
-    background: rgba(0, 0, 0, 0.6);
-    color: #fff;
-  }
-`
-const InvalidButton = styled(Button)`
-  background: rgba(0, 0, 0, 0.6);
-  color: #fff;
-  &:hover {
-    background: #49ffa1;
-    color: #000;
+  transition: all 0.3s ease-in-out;
+  &:hover ${Button} {
+    opacity: 1;
   }
 `
 
@@ -56,7 +58,7 @@ const ActivityCard: React.FunctionComponent<ActivityCardProps> = (props) => {
   return (
     <ActivityCardWrap>
       <Title>{props.title}</Title>
-      {props.valid ? <Button>{t('Participate Now')}</Button> : <InvalidButton>{t(`View Event`)}</InvalidButton>}
+      {props.valid ? <Button>{t('Participate now')}</Button> : <InvalidButton>{t(`View Event`)}</InvalidButton>}
     </ActivityCardWrap>
   )
 }
