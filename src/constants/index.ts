451ae25bb6d5235f3133f2e584c7ffba8a06995b
momdoge <87006351+momdoge@users.noreply.github.com>
2021-07-18 12:16:18 +0700

    up

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,8 +2,6 @@ export const KCC = {
   FULLNAME: 'MomDoge Community Chain',
   ABBR: 'KCC',
   EXPLORER: 'https://explorer.kcc.io',
-  TEST_EXPLORER: '/',
-  FAUCET: '/',
   DOCS_URL: 'https://app.momdoge.finance',
   GITHUB_URL: 'https://github.com/MomDoge',
   DAPP_URL: 'https://github.com/MomDoge',
